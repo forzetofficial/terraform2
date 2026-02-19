7ca2175a00e88209e48430953cf5370a0153c811
Леонид <alsaoff@yandex.ru>
2026-02-19 22:13:02 +0300

    test4

--- vms/variables.tf
+++ vms/variables.tf
@@ -2,5 +2,5 @@
 
 variable "public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiVcfW8Wa/DxbBNzmQcwn7hJOj7ji9eoTpFakVnY/AI webinar"
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMLfJOmjJr2h7dTOLu2b8pJ2TseHNEh0+vUyR/wtE9s netol@alsa"
 }
