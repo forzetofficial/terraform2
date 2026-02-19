6b9ef985612db894de41a88ac53731f0e1779914
Леонид <alsaoff@yandex.ru>
2026-02-19 22:43:45 +0300

    test11

--- vms/variables.tf
+++ vms/variables.tf
@@ -21,3 +21,45 @@ variable "labels" {
   description = "Метки для ВМ"
   default     = {}
 }
+
+# Семейство образа (для data "yandex_compute_image")
+variable "image_family" {
+  type        = string
+  description = "Семейство образа ОС"
+  default     = "ubuntu-2004-lts"  # Значение по умолчанию
+}
+
+variable "platform_id" {
+  type        = string
+  default     = "standard-v1"
+}
+
+variable "zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "cores" {
+  type        = number
+  default     = 2
+}
+
+variable "memory" {
+  type        = number
+  default     = 2
+}
+
+variable "core_fraction" {
+  type        = number
+  default     = 5
+}
+
+variable "disk_size" {
+  type        = number
+  default     = 10
+}
+
+variable "disk_type" {
+  type        = string
+  default     = "network-hdd"
+}
