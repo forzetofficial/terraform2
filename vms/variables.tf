7c8b464c8f1b12087e8f8e803a643d75215df7d9
Леонид <alsaoff@yandex.ru>
2026-02-19 22:28:16 +0300

    test6

--- vms/variables.tf
+++ vms/variables.tf
@@ -1,6 +1,24 @@
-###cloud vars
+# Имя ВМ
+variable "vm_name" {
+  type        = string
+  description = "Имя виртуальной машины"
+}
 
-variable "public_key" {
-  type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMLfJOmjJr2h7dTOLu2b8pJ2TseHNEh0+vUyR/wtE9s netol@alsa"
+# ID подсети
+variable "subnet_id" {
+  type        = string
+  description = "ID подсети"
+}
+
+# SSH-ключ (переименуем для ясности, можно оставить и public_key)
+variable "ssh_public_key" {
+  type        = string
+  description = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMLfJOmjJr2h7dTOLu2b8pJ2TseHNEh0+vUyR/wtE9s netol@alsa"
+}
+
+# Метки
+variable "labels" {
+  type        = map(string)
+  description = "Метки для ВМ"
+  default     = {}
 }
